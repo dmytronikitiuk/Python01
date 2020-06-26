8b925d78f8fe555505bba627afce3678c818d5f0
dmytronikitiuk <dmytronikitiuk140700@gmail.com>
2020-06-26 23:04:52 +0300

    ToE - done

---
+++ HW2/ToE.py
@@ -0,0 +1,20 @@
+
+lin1 = r"╭------------------------------------------------------╮"
+str1 = r"| \a    Bell (alert)           |  Hello *beep* world!  |"
+str2 = r"| \b    Backspace              |  Helloworld!          |"
+str3 = r"| \n    New line               |  Hello                |"
+str4 = r"|                              |  world!               |"
+str5 = r"| \t    Horizontal tab         |  Hello   world!       |"
+str6 = r"| \\    Backslash \            |  Hello \ world!       |"
+str7 = r'| \"    Double quotation mark  |  Hello " world!       |'
+str8 = r"| \'    Single quotation mark  |  Hello ' world!       |"
+lin2 = r"╰------------------------------------------------------╯"
+print(lin1)
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+print(str6)
+print(str7)
+print(lin2)
